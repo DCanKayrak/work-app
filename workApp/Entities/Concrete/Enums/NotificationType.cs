eb8e8f9dabe4ce667dc30e973dca2dc8e509f893
DANYAL CAN KAYRAK <dancankan@gmail.com>
2024-06-04 16:46:46 +0300

    chore: resetted changes

---
+++ workApp/Entities/Concrete/Enums/NotificationType.cs
@@ -0,0 +1,8 @@
+﻿namespace Entities.Concrete.Enums;
+
+public enum NotificationType
+{
+    FOLLOW_REQUEST = 0,
+    FOLLOW_ACCEPTED = 1,
+    
+}
---
+++ workApp/Entities/Concrete/Notification.cs
@@ -0,0 +1,11 @@
+﻿using Entities.Concrete.Enums;
+
+namespace Entities.Concrete;
+
+public class Notification : BaseEntity
+{
+    public int UserId { get; set; }
+    public string Message { get; set; }
+    public bool IsRead { get; set; } = false;
+    public NotificationType NotificationType { get; set; }
+}
